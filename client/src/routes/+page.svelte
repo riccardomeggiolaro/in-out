20f477d2c030aa411e4ab5aad3e4b00129545291
riccardomeggiolaro <riccardomeggiolaro.info@protonmail.com>
2024-09-19 17:45:46 +0200

    visore template created

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,57 +1,117 @@
-<script>
-	import Counter from './Counter.svelte';
-</script>
-
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Dashboard</title>
+	<meta name="description" content="About this app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset='/assets/images/svelte-welcome.webp' type="image/webp" />
-				<img src='/assets/images/svelte-welcome.webp' alt="Welcome" />
-			</picture>
-		</span>
+<div class="content">
+    <div>
+        <span>000000</span>
+        <span>kg</span>
+    </div>
+    <div>
+        <div>
+            000000
+        </div>
+        <div>
+            ST
+        </div>
+    </div>
+</div>
 
-		to your new<br />SvelteKit app
-	</h1>
+<div class="banner">
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+    div {
+        border: 2px solid black;
+        border-radius: 15px;
+        margin: 3px;
+    }
 
-	h1 {
-		width: 100%;
-	}
+    .content,
+    .banner {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-content: center;
+    }
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+    .content,
+    .banner {
+        border: 0px;
+        margin: 0px;        
+    }
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    .content > div:last-child {
+        border: 0px;
+        margin-bottom: 0px;
+        margin-top: 0px;
+    }
+
+    .content {
+        height: 50vh;
+    }
+
+    .banner {
+        height: 50vh;
+    }
+
+    .content div:first-child {
+        width: 75%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+    }
+
+    .content div:first-child span:first-child {
+        font-size: 20vw;
+        font-weight: bold;
+        margin-bottom: 80px !important;
+    }
+
+    .content div:first-child span:last-child {
+        position: absolute; 
+        bottom: 0; 
+        right: 0; 
+        font-size: 4.5vw;
+        font-weight: 500;
+    }
+
+    .content div:last-child {
+        width: 25%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .content div:first-child div:last-child {
+        width: 100%;
+        height: 30vh;
+        font-size: 2.5vw;
+        font-weight: 500;
+    }
+    
+    .content div:last-child div {
+        width: 100%;
+        height: 50vh;
+        font-size: 6.5vw;
+        font-weight: 500;
+    }
+
+    table {
+        border: 1px solid black;
+        width: 100%;
+    }
+
+    td div {
+        width: 10px;
+        height: 10px;
+        background-color: green;
+        border-radius: 100%;
+    }
 </style>
