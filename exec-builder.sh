cddf83306b98f7d9c045302e863054e9e0563a40
riccardomeggiolaro <riccardomeggiolaro.info@protonmail.com>
2024-10-15 12:02:32 +0200

    fixed

--- exec-builder.sh
+++ exec-builder.sh
@@ -1,8 +1,19 @@
 #/usr/bin/bash
-source venv/bin/activate
+source .env/bin/activate
 pip install -r requirements.txt
 # Linux
-pyinstaller --onefile --add-data "config.json:." --add-data "service.log:."  main.py
+pyinstaller --onefile \
+    --add-data "config.json:." \
+    --add-data "service.log:." \
+    --add-data "client/dashboard.html:./client/" \
+    --add-data "client/index.html:./client/" \
+    main.py
 # Windows
-python -m PyInstaller --onefile --add-data "config.json:." --add-data "service.log:."  main.py
+python -m PyInstaller --onefile \
+    --add-data "config.json:." \
+    --add-data "service.log:." \
+    --add-data "client/dashboard.html:./client/" \
+    --add-data "client/index.html:./client/" \
+    main.py
+
 ./dist/main
