f68a210563cf5a9994ee9382ebf6a6477aeacad7
RiccardoMeggiolaro <100524624+riccardomeggiolaro@users.noreply.github.com>
2025-06-26 09:00:08 +0200

    Update router.py

--- applications/router/weigher/router.py
+++ applications/router/weigher/router.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
 from applications.router.weigher.command_weigher import CommandWeigherRouter
-from applications.router.weigher.cams import CamsRouter
-from applications.router.weigher.rele import ReleRouter
 
 class WeigherRouter:
 	def __init__(self):
@@ -9,16 +7,8 @@ class WeigherRouter:
 
 		command_weigher = CommandWeigherRouter()
 
-		cams = CamsRouter()
-  
-		rele = ReleRouter()
-
 		self.router.include_router(command_weigher.router_action_weigher, prefix='/command-weigher', tags=['actions weigher'])
 	
-		self.router.include_router(cams.router_cams, prefix='/cams', tags=['cams'])		
-
-		self.router.include_router(rele.router_rele, prefix='/rele', tags=['rele'])
- 
 		self.router.include_router(command_weigher.router_data, prefix='/data', tags=['data'])
  
 		self.router.include_router(command_weigher.router_config_weigher, prefix='/config-weigher', tags=['config weigher'])
