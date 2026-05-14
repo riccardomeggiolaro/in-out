cb660e44fceeaa8065390e5af0340ba28db0f76f
riccardo <riccardomeggiolaro.info@protonmail.com>
2026-05-14 14:14:33 +0200

    fixed

--- applications/router/weigher/callback_weigher.py
+++ applications/router/weigher/callback_weigher.py
@@ -344,7 +344,7 @@ class CallbackWeigher(Functions, WebSocket):
 					modope = "CLOSERELE" if rele["set"] == 0 else "OPENRELE"
 					rele_status = lb_config.g_config["app_api"]["weighers"][instance_name]["nodes"][weigher_name]["rele"].get(rele["rele"], 0)
 					r = md_weigher.module_weigher.setModope(instance_name=instance_name, weigher_name=weigher_name, modope=modope, port_rele=(rele["rele"], rele_status))
-					lb_log.error(r)
+					lb_log.info(f"Settaggio relè {rele['rele']} in modalità {modope} con stato {rele_status} per evento {event_type}: {r}")
 					time.sleep(time_between_actions)
 		elif not last_pesata.weight_executed.executed:
 			if last_pesata.data_assigned.accessId and access.hidden is True:
